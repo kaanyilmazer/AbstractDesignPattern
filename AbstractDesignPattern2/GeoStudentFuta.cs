de0be6473027087ee091b05d596cae6f7a39b70f
kaan <iamyilmazer@gmail.com>
2022-07-19 10:32:28 +0300

    geostudeneklendi

---
+++ AbstractDesignPattern2/GeoStudentFuta.cs
@@ -0,0 +1,15 @@
+﻿using AbstractDesignPattern2.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractDesignPattern2
+{
+    public class GeoStudentFuta : IStudent
+    {
+        public List<string> Courses()
+        {
+            return new List<string> { "Geo 101", "Chem 201" };
+        }
+    }
+}
